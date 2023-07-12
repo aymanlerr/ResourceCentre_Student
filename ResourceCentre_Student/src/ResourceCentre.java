d19a74d126ee99340a5cd84eb6de5150b0d58187
chavonne-kek <22017979@myrp.edu.sg>
2023-07-12 11:37:36 +0800

    addChromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -191,7 +191,19 @@ public class ResourceCentre {
 	}	
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
 		// write your code here
+		Chromebook item;
+		for(int i = 0; i < chromebookList.size(); i++) {
+			item = chromebookList.get(i);
+			if (item.getAssetTag().equalsIgnoreCase(cb.getAssetTag()) )
+				return;
+		}
+		if ((cb.getAssetTag().isEmpty()) || (cb.getDescription().isEmpty()) ) {
+			return;
+		}
+		
+		chromebookList.add(cb);
 	}
+	
 	
 	//================================= Option 3 Loan an item (CRUD - Update) =================================
 	public static boolean doLoanCamcorder(ArrayList<Camcorder> camcorderList, String tag, String dueDate) {
